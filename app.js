d73d11043cdab0921b47a80113be74c7d38b03d8
Shahmeer09 <shahmeerrizwan921@gmail.com>
2023-10-28 20:34:15 +0500

    practice

---
+++ app.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("#head");
+h1.innerHTML = "shahmeer"
+
+
+function greetuser (){
+    if(h1.innerHTML === "changed through java script")
+    { h1.innerHTML= "hello world"}
+    else{h1.innerHTML = "changed through java scripts"}
+
+console.log("hello world") }
