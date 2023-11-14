43f7a267bf7e171025e0b966921e043dfa715d38
shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-14 20:46:31 +0500

    practice

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const h1 = document.querySelector("#head");
-h1.innerHTML = "shahmeer"
+// const h1 = document.querySelector("#head");
+// h1.innerHTML = "shahmeer"
 
 
-function greetuser (){
-    if(h1.innerHTML === "changed through java script")
-    { h1.innerHTML= "hello world"}
-    else{h1.innerHTML = "changed through java scripts"}
+// function greetuser (){
+//     if(h1.innerHTML === "changed through java script")
+//     { h1.innerHTML= "hello world"}
+//     else{h1.innerHTML = "changed through java scripts"}
 
-console.log("hello world") }
+// console.log("hello world") }
+
+
+
+// DOM MANIPULATION :
+
+
+const mybody = document.body;
+mybody.style.backgroundColor = 'yellow'
+
+
+const box2 = document.getElementById('box-2');
+console.log(box2);
