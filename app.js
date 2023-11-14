60852c248399a4136a172a53018446ea5e6b90af
shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-14 21:39:34 +0500

    practice

--- app.js
+++ app.js
@@ -14,15 +14,22 @@
 // DOM MANIPULATION :
 
 
-const mybody = document.body;
-mybody.style.backgroundColor = 'yellow'
+// const mybody = document.body;
+// mybody.style.backgroundColor = 'yellow'
 
 
-const box1 = document.querySelector('.box');
-console.log(box1);
+// const box1 = document.querySelector('.box');
+// console.log(box1);
 
-const box2 = document.getElementById('box-2');
-console.log(box2);
-box2.innerHTML = 'shah';
+// const box2 = document.getElementById('box-2');
+// console.log(box2);
+//  box2.innerHTML = '<h1>shah</h1>';
 
 
+// box1.style.backgroundColor = 'red';
+// box1.classList.add('round-border');
+
+// const boxes = document.getElementsByClassName('.box');
+// for(let i = 0; i<boxes.length; i++) {
+//     boxes[i].classList.add('round-border');
+// }
